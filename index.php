d80ee0451f789e141a34dd350265aca337bff344
wuyue <wuyue@singbada.cn>
2022-02-28 10:02:43 +0800

    test

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Index.php
+ * @author: wuyue
+ * @date: 2022/2/26 18:37
+ */
+
+require "vendor/autoload.php";
+
+(new Yanxi\Index())->index();
---
+++ src/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Index.php
+ * @author: wuyue
+ * @date: 2022/2/26 18:34
+ */
+
+namespace Yanxi;
+
+
+class Index
+{
+    public function index()
+    {
+        echo '索嗨';
+    }
+}
